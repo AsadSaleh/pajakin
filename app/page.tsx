9fb6959bd2dc619b2bd2e543b56afa7fa073f15e
As'ad Saleh Umar <azzzad.s@gmail.com>
2023-08-20 22:57:42 +0700

    Create pajakin

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,273 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
+import { NumericFormat, numericFormatter } from "react-number-format";
+
+type ProgressiveTax = {
+  batasBawah: number;
+  batasAtas: number;
+  persentasePajak: number;
+};
+
+type PajakTerhitung = {
+  besaranKenaPajak: number;
+  pphTerutang: number;
+};
+
+const ptkpKategori = {
+  "TK/0": {
+    desc: "Tidak Kawin Tanpa Tanggungan",
+    tarif: 54_000_000,
+  },
+  "TK/1": {
+    desc: "Tidak Kawin 1 Tanggungan",
+    tarif: 58_500_000,
+  },
+  "TK/2": {
+    desc: "Tidak Kawin 2 Tanggungan",
+    tarif: 63_000_000,
+  },
+  "TK/3": {
+    desc: "Tidak Kawin 3 Tanggunan",
+    tarif: 67_500_000,
+  },
+  "K/0": { tarif: 58_500_000, desc: "Kawin Tanpa Tanggungan" },
+  "K/1": { tarif: 63_000_000, desc: "Kawin 1 Tanggungan" },
+  "K/2": { tarif: 67_500_000, desc: "Kawin 2 Tanggungan" },
+  "K/3": { tarif: 72_000_000, desc: "Kawin 3 Tanggungan" },
+  "K/I/0": {
+    tarif: 112_500_000,
+    desc: "Kawin dan Istri Digabung, Tanpa Tanggungan",
+  },
+  "K/I/1": {
+    tarif: 117_000_000,
+    desc: "Kawin dan Istri Digabung, 1 Tanggungan",
+  },
+  "K/I/2": {
+    tarif: 121_500_000,
+    desc: "Kawin dan Istri Digabung, 2 Tanggungan",
+  },
+  "K/I/3": {
+    tarif: 126_000_000,
+    desc: "Kawin dan Istri Digabung, 3 Tanggungan",
+  },
+};
+
+/**
+ * Note: batasBawah is inclusive and batasAtas is exclusive.
+ */
+const progressiveTaxes: ProgressiveTax[] = [
+  {
+    batasBawah: 0,
+    batasAtas: 60_000_000,
+    persentasePajak: 0.05,
+  },
+  {
+    batasBawah: 60_000_000,
+    batasAtas: 250_000_000,
+    persentasePajak: 0.15,
+  },
+  {
+    batasBawah: 250_000_000,
+    batasAtas: 500_000_000,
+    persentasePajak: 0.25,
+  },
+  {
+    batasBawah: 500_000_000,
+    batasAtas: 5_000_000_000,
+    persentasePajak: 0.3,
+  },
+  {
+    batasBawah: 5_000_000_000,
+    batasAtas: Infinity,
+    persentasePajak: 0.35,
+  },
+];
+
+function calculateProgressiveTax(pkp: number) {
+  let newArr: Array<ProgressiveTax & PajakTerhitung> = [];
+  for (let i = 0; i < progressiveTaxes.length; i++) {
+    const cur = progressiveTaxes[i];
+
+    if (pkp < cur.batasBawah) {
+      newArr.push({
+        ...cur,
+        pphTerutang: 0,
+        besaranKenaPajak: 0,
+      });
+    }
+    if (pkp >= cur.batasAtas) {
+      const besaranKenaPajak = cur.batasAtas - cur.batasBawah;
+      newArr.push({
+        ...cur,
+        pphTerutang: besaranKenaPajak * cur.persentasePajak,
+        besaranKenaPajak,
+      });
+    }
+
+    if (pkp >= cur.batasBawah && pkp < cur.batasAtas) {
+      const besaranKenaPajak = pkp - cur.batasBawah;
+      newArr.push({
+        ...cur,
+        pphTerutang: besaranKenaPajak * cur.persentasePajak,
+        besaranKenaPajak,
+      });
+    }
+  }
+
+  return newArr;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [bruto, setBruto] = useState<string>();
+  const [ptkpKey, setPtkpKey] = useState<keyof typeof ptkpKategori>("TK/0");
+  const penghasilanBrutoTahunan = Number(bruto ?? "0") * 12;
+  const ptkp = ptkpKategori[ptkpKey].tarif;
+  const pkp =
+    penghasilanBrutoTahunan - ptkp < 0 ? 0 : penghasilanBrutoTahunan - ptkp;
+  const calculatedProgressiveTaxes = calculateProgressiveTax(pkp);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const pphTerutangPertahun = calculatedProgressiveTaxes.reduce(
+    (a, c) => c.pphTerutang + a,
+    0
+  );
+  const pphTerutangPerbulan = pphTerutangPertahun / 12;
+
+  console.log({ calculatedProgressiveTaxes });
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+      <h1 className="text-xl mb-10">
+        Pajakin: Aplikasi penghitung pajak gratis
+      </h1>
+
+      <div className="flex gap-3 items-center">
+        <label className="text-slate-300">Gaji Bruto Bulanan</label>
+        <NumericFormat
+          placeholder="gaji bruto bulanan"
+          className="bg-slate-800 py-1 px-2 rounded-lg disabled:bg-black"
+          thousandSeparator="."
+          decimalSeparator=","
+          prefix="Rp"
+          value={bruto}
+          onValueChange={(e) => setBruto(e.value)}
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex gap-3 items-center">
+        <label className="text-slate-300">Gaji Bruto Tahunan</label>
+        <NumericFormat
+          placeholder="gaji bruto bulanan"
+          className="bg-slate-800 py-1 px-2 rounded-lg disabled:bg-black"
+          thousandSeparator="."
+          decimalSeparator=","
+          prefix="Rp"
+          value={penghasilanBrutoTahunan}
+          readOnly
+          disabled
+        />
       </div>
+
+      <div className="flex gap-3 items-center">
+        <label className="text-slate-300">PTKP</label>
+        <select
+          className="bg-slate-800 py-1 px-2 rounded-lg"
+          onChange={(v) =>
+            setPtkpKey(v.target.value as keyof typeof ptkpKategori)
+          }
+        >
+          {Object.entries(ptkpKategori).map(([key, value]) => (
+            <option key={key} value={key}>
+              {value.desc}
+            </option>
+          ))}
+        </select>
+        <NumericFormat
+          placeholder="gaji bruto bulanan"
+          className="bg-slate-800 py-1 px-2 rounded-lg disabled:bg-black"
+          thousandSeparator="."
+          decimalSeparator=","
+          prefix="Rp"
+          value={ptkp}
+          readOnly
+          disabled
+        />
+      </div>
+
+      <div className="flex gap-3 items-center">
+        <label className="text-slate-300">PKP</label>
+        <NumericFormat
+          placeholder="gaji bruto bulanan"
+          className="bg-slate-800 py-1 px-2 rounded-lg disabled:bg-black"
+          thousandSeparator="."
+          decimalSeparator=","
+          prefix="Rp"
+          value={pkp}
+          readOnly
+          disabled
+        />
+      </div>
+
+      <table className="border w-[900px]">
+        <thead>
+          <tr className="py-1 px-2 text-lg border-b-2 border-slate-400">
+            <th className="tpy-1 px-2 text-left">Pajak Progresif</th>
+            <th className="py-1 px-2">Percentage</th>
+            <th className="py-1 px-2 text-right">Besaran Kena Pajak</th>
+            <th className="py-1 px-2 text-right">PPh Terutang</th>
+          </tr>
+        </thead>
+        <tbody>
+          {calculatedProgressiveTaxes.map((calcProgTax, i) => {
+            return (
+              <tr key={i}>
+                <td className="py-1 px-2">
+                  Ceiling&nbsp;
+                  {formatCurrency(calcProgTax.batasAtas)}
+                </td>
+
+                <td className="py-1 px-2 text-center">
+                  {calcProgTax.persentasePajak * 100}%
+                </td>
+                <td className="py-1 px-2 text-right">
+                  {formatCurrency(calcProgTax.besaranKenaPajak)}
+                </td>
+                <td className="py-1 px-2 text-right">
+                  {formatCurrency(calcProgTax.pphTerutang)}
+                </td>
+              </tr>
+            );
+          })}
+          <tr className="border-t-2 border-slate-400">
+            <td colSpan={3} className="py-1 px-2">
+              Pph Terutang per tahun
+            </td>
+            <td className="text-right py-1 px-2">
+              {formatCurrency(pphTerutangPertahun)}
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={3} className="py-1 px-2">
+              PPh Terutang per bulan
+            </td>
+            <td className="text-right py-1 px-2">
+              {formatCurrency(pphTerutangPerbulan)}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </main>
-  )
+  );
+}
+
+function formatCurrency(input: number | string) {
+  input = input.toString();
+  if (input === "0") {
+    return "0";
+  }
+  return numericFormatter(input, {
+    prefix: "Rp",
+    thousandSeparator: ".",
+    decimalSeparator: ",",
+    decimalScale: 2,
+  });
 }
